1415fa269c7c3658080606280233f39dea4d776e
Rupesh Shrestha <rupeshshrestha1439@gmail.com>
2023-02-12 00:53:28 +0545

    feat: add scroll event listener to do pagination

---
+++ src/hooks/useGetScrollDirection.ts
@@ -0,0 +1,42 @@
+'use client';
+import { useState, useEffect } from 'react';
+
+export default function useGetScrollDirection() {
+    const threshold = 100;
+    const [scrollDir, setScrollDir] = useState<'up' | 'down'>('up');
+
+    useEffect(() => {
+        let previousScrollYPosition = window.scrollY;
+
+        const scrolledMoreThanThreshold = (currentScrollYPosition: number) =>
+            Math.abs(currentScrollYPosition - previousScrollYPosition) >
+            threshold;
+
+        const isScrollingUp = (currentScrollYPosition: number) =>
+            currentScrollYPosition > previousScrollYPosition &&
+            !(previousScrollYPosition > 0 && currentScrollYPosition === 0) &&
+            !(currentScrollYPosition > 0 && previousScrollYPosition === 0);
+
+        const updateScrollDirection = () => {
+            const currentScrollYPosition = window.scrollY;
+
+            if (scrolledMoreThanThreshold(currentScrollYPosition)) {
+                const newScrollDirection = isScrollingUp(currentScrollYPosition)
+                    ? 'down'
+                    : 'up';
+                setScrollDir(newScrollDirection);
+                previousScrollYPosition =
+                    currentScrollYPosition > 0 ? currentScrollYPosition : 0;
+            }
+        };
+
+        const onScroll = () =>
+            window.requestAnimationFrame(updateScrollDirection);
+
+        window.addEventListener('scroll', onScroll);
+
+        return () => window.removeEventListener('scroll', onScroll);
+    }, []);
+
+    return scrollDir;
+}
