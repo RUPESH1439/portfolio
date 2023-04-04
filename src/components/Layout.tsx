3de664a3116091a33533322417d29f75484ed4f3
Rupesh Shrestha <rupesh.shrestha96742@gmail.com>
2023-04-04 11:38:41 +0545

    fix: fix hydration error

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,21 +6,13 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-            {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-            <body>
-                <div
-                    className="flex flex-1 flex-col bg-background w-full h-[100%] min-h-screen"
-                    style={{ scrollBehavior: 'smooth' }}
-                >
-                    <div className="h-[200px] w-[200px] md:w-[700px] blur-[400px] bg-blue-gradient rounded-full absolute -top-[10px] left-[25vw]" />
-                    <NavBar />
-                    <div className="">{children}</div>
-                </div>
-            </body>
-        </html>
+        <div
+            className="flex flex-1 flex-col bg-background w-full h-[100%] min-h-screen"
+            style={{ scrollBehavior: 'smooth' }}
+        >
+            <div className="h-[200px] w-[200px] md:w-[700px] blur-[400px] bg-blue-gradient rounded-full absolute -top-[10px] left-[25vw]" />
+            <NavBar />
+            <div className="">{children}</div>
+        </div>
     );
 }
